4862042c9f2347ac8d9b7294b21b8c88044a5ab0
Keith Ryan Wong <KeithRyanWong@gmail.com>
2017-08-30 14:24:12 -0700

    Delete Front-End files

--- front-end/actions/index.js
+++
@@ -1,5 +0,0 @@
-export * from './auth_actions';
-export * from './alert_actions';
-export * from './todo_actions';
-export * from './user_actions';
-export * from './plant_actions';
--- front-end/assets/spritesheets/background/background.js
+++
@@ -1,8 +0,0 @@
-const BACKGROUND = {
-   morning: require('./default/sun1.png'),
-   afternoon: require('./default/sun2.png'),
-   evening: require('./default/sun3.png'),
-   night: require('./default/night.png'),
- };
-
- export default BACKGROUND;
